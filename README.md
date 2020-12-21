cba69020d29560e2a37728c0832f0f99638a26eb
FJB-ouweDremelaar <ferrie.bank@gmail.com>
2020-12-21 16:23:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# LCD screen news ticker
+
+An LCD screen used for display of news headlines, loaded from internet.
+A NodeMCU ESP8266 12-F board drives the display, and loads the texts periodically from internet via WiFi.
+An AWS Elastic Beanstalk Spring Boot application provides the input texts to be displayed. (see the 'scrapert' project on https://github.com/oudesnepelaar).
+
+Parts:
+ - NodeMCU ESP8266 12-F
+ - .ino Sketch file to program the ESP8266
+ - LCD screen text display
+ - power switch
+ - micro USB connection board for power delivery
+
+Ferrie J Bank,
+Amsterdam 21 December 2020
+
+https://github.com/oudesnepelaar
---
+++ lcd-news-ticker.ino
@@ -0,0 +1,97 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <MD_MAX72xx.h>
+#include <MD_Parola.h>
+#include <SPI.h>
+ 
+const char* ssid = "ZiggoA72811A";
+const char* password = "E3asv6vauwdk";
+
+#define HARDWARE_TYPE MD_MAX72XX::PAROLA_HW
+#define MAX_DEVICES 4
+
+#define CLK_PIN   D5
+#define CS_PIN    D8
+#define DATA_PIN  D7
+
+MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+MD_MAX72XX *MAX = P.getGraphicObject();
+
+uint8_t scrollSpeed = 35;
+textEffect_t scrollEffect = PA_SCROLL_LEFT;
+textPosition_t scrollAlign = PA_LEFT;
+
+int const bufferSize = 8192;
+char myBuffer[bufferSize] = {"Welcome to the News Ticker :-) by Ferrie J Bank (c) 2020"};
+String payload;
+boolean bufferDirty = false;
+
+unsigned long lastReadTime = 0;
+unsigned long readTimeDelay = 600000; // refresh news every 10 minutes
+ 
+void setup () {
+
+  displayInit();
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+   
+    delay(500);
+  }
+}
+
+void displayInit() {
+
+  P.begin();
+  P.displayClear();
+  P.displaySuspend(false);
+  P.setIntensity(0);
+
+  P.displayScroll(myBuffer, scrollAlign, scrollEffect, scrollSpeed);
+  
+  while (!P.displayAnimate()) {yield();}
+}
+
+
+void loop() {
+  
+  readNews();
+  
+  if (bufferDirty) {
+
+    bufferDirty = false;
+
+    MAX->clear();
+    payload.toCharArray(myBuffer, bufferSize);
+    P.displayReset();
+  }
+
+  if (P.displayAnimate()) {
+    P.displayReset();
+  }
+}
+
+void readNews() {
+
+  unsigned long diff = millis() - lastReadTime;
+  if ((lastReadTime > 0) && diff < readTimeDelay) return;
+  
+  lastReadTime = millis();
+
+  if (WiFi.status() == WL_CONNECTED) {
+     
+    HTTPClient http;
+     
+    http.begin("http://scrapert-env-1.eba-czz2mtaz.us-east-2.elasticbeanstalk.com/mix");
+    int httpCode = http.GET();
+     
+    if (httpCode > 0) {
+       
+      payload = http.getString();
+      bufferDirty = true;
+    }
+     
+    http.end();
+  }
+}
