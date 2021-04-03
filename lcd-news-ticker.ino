96bea5518aaf8eebcc984af859dcb8d7436348f5
FJB-ouweDremelaar <ferrie.bank@gmail.com>
2021-04-03 13:11:39 +0200

    implemented suitable scrolling mechanism

--- lcd-news-ticker.ino
+++ lcd-news-ticker.ino
@@ -10,11 +10,14 @@ AutoConnect Portal(Server);
 
 String payload = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 boolean bufferDirty = false;
+
+unsigned int widthChars = 16;
+unsigned int heightChars = 2;
 unsigned int displayCharIndex = 0;
-unsigned int delayms = 1000;
+unsigned int delayms = 350;
 
 unsigned long lastReadTime = 0;
-unsigned long readTimeDelay = 600000; // refresh news every 10 minutes (= 60 * 10 * 1000 ms)
+unsigned long refreshDelay = 1200000; // refresh news every 20 minutes (= 60 * 10 * 1000 ms)
 
 LiquidCrystal lcd(13, 15, 5, 4, 0, 2);
 
@@ -27,29 +30,32 @@ void rootPage() {
 void setup () {
 
   delay(1000);
-
-  Serial.begin(115200);
-  Serial.println();
-
   Server.on("/", rootPage);
-  if (Portal.begin()) {
-    Serial.println("WiFi connected: " + WiFi.localIP().toString());
-  }
-  
   displayInit();
+  printIdent();
 }
 
 void displayInit() {
 
-  lcd.begin(16, 2);
-  lcd.autoscroll();
-  lcd.setCursor(16, 1);
-  lcd.print(payload);
+  lcd.begin(widthChars, heightChars);
+  lcd.clear();
 }
 
+void printIdent() {
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Ferrie Bank 2021");
+
+  lcd.setCursor(0, 1);
+  if (Portal.begin()) {
+    lcd.print(WiFi.localIP().toString());
+  } else {
+    lcd.print("plz connect AP");
+  }
+}
 
 void loop() {
-
+  
   Portal.handleClient();
   readNews();
   
@@ -61,28 +67,26 @@ void loop() {
     lcd.print(payload);
   }
 
-  printNextChar();
+  advanceScroll();
   delay(delayms);
 }
 
-void printNextChar() {
+void advanceScroll() {
 
-  lcd.setCursor(16, 1); // lower row
-  lcd.print(payload.charAt(displayCharIndex));
+  lcd.setCursor(0, 0);
+  lcd.print(payload.substring(displayCharIndex, displayCharIndex + widthChars));
 
-  if (displayCharIndex > 15) {
-    lcd.setCursor(16, 0); // upper row
-    lcd.print(payload.charAt(displayCharIndex - 16));
-  }
+  lcd.setCursor(0, 1);
+  lcd.print(payload.substring(displayCharIndex + widthChars, displayCharIndex + widthChars + widthChars));
 
   displayCharIndex++;
-  if (displayCharIndex == payload.length()) displayCharIndex = 0;
+  if (displayCharIndex == (payload.length() - widthChars - widthChars)) displayCharIndex = 0;
 }
 
 void readNews() {
 
   unsigned long diff = millis() - lastReadTime;
-  if ((lastReadTime > 0) && diff < readTimeDelay) return;
+  if ((lastReadTime > 0) && diff < refreshDelay) return;
   
   lastReadTime = millis();
 
